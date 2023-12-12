e754302083a6e329727157f5061a4540f8700d07
holsterteehee <104798451+holsterteehee@users.noreply.github.com>
2023-12-11 20:26:56 -0800

    dumb settings

--- Assets/Scripts/DamageSource.cs
+++ Assets/Scripts/DamageSource.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DamageSource : MonoBehaviour
 {
-    [SerializeField] private int damageAmount = 1;
+    [SerializeField] private int damageAmount = 3;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
